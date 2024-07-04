71ef686ac0aa6a5627668d8c35ff51d135954363
Mcf11 <michel.felipe0@hotmail.com>
2024-07-04 00:58:21 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Te amo amoreco, se você me ama da uma risadinha.</h1>
+    return <h1>Te amo amoreco, se você me ama da uma risadinha. 🏄‍♂️ </h1>
 }
 
 export default Home;
